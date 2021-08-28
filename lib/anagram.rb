b7640e44bd2d3ec5bed6991db2c0f94f8bb29316
Mendel <mkonikov123@gmail.com>
2021-08-28 11:55:09 -0400

    commit tree 596bcd2929c9eac6270fdf6ab4b9969546c4cedb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram 
+
+    attr_accessor :word, :match
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        new_arr = []
+        array.each do |e|
+            if e.chars.sort == @word.chars.sort
+                new_arr.push(e)
+            end
+        end
+        return new_arr
+    end
+end
